5798f8ba96b6d42eea7033150a8b3f940cdce917
pavel <A1XP>
2026-01-30 16:49:03 +0100

    ex00

---
+++ ex00/main.cpp
@@ -0,0 +1,24 @@
+#include "BitcoinExchange.hpp"
+#include <iostream>
+
+int main (int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <data_file>" << std::endl;
+        return 1;
+    }
+
+    try
+    {
+        BitcoinExchange btcExchange;
+        btcExchange.processFile(argv[1]);
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
